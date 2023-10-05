31e4286dd93393acd31cdb88bff90931c04ff2ce
karxg <karim44230@gmail.com>
2023-10-05 08:19:59 -0700

    9th Task Fix1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,9 @@
 
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, strlen(msg));
+
 	return (1);
 }
