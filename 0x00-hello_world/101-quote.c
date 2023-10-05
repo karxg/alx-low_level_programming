3764d673a359974ae309d6b702e0e51201a20b8e
karxg <karim44230@gmail.com>
2023-10-05 08:10:03 -0700

    9th Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Program point
+ *
+ * Description: Using char function to print a sentecne
+ *
+ * Return: 1 (failed)
+*/
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);
+}
