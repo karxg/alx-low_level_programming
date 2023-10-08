fa4a49cd47d1bb818cbfa28386ecc9981bb9e2a7
karxg <karim44230@gmail.com>
2023-10-08 18:28:32 +0300

    fix4

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,21 +11,26 @@
  * Return: Always 0 (Success)
 */
 
+#include <stdio.h>
+
 int main(void)
 {
-	for (char num1 = '0'; num1 <= '9'; num1++)
-	{
-		for (char num2 = num1 + 1; num2 <= '9'; num2++)
-		{
-			putchar(num1);
-			putchar(num2);
-			if (num1 != '8' || num2 != '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+char num1, num2;
+
+for (num1 = '0'; num1 <= '9'; num1++)
+{
+for (num2 = num1 + 1; num2 <= '9'; num2++)
+{
+putchar(num1);
+putchar(num2);
+if (num1 != '8' || num2 != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+return (0);
 }
