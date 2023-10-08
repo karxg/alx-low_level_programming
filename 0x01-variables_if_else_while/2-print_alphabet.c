51c58d1b001c1e065afbd11ab5fcd6728d0e71e1
karxg <karxg1@gmail.com>
2023-10-08 08:39:49 +0300

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description:program that prints the aphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	do
+    {putchar(ch);
+    ch++;
+    } while (ch <= 'z');
+    putchar('\n');
+
+	return (0);
+}
