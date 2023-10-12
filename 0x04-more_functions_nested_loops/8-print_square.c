39472d7c04a8038cb0167529505c6fe70a76879c
karxg <karim44230@gmail.com>
2023-10-12 13:46:10 +0300

    Fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; ++i)
+	for (i = 1; i < size; ++i)
 	{
-		for (j = 1; j <= size; ++j)
+		for (j = 1; j < size; ++j)
 			_putchar('#');
 		_putchar('\n');
 	}
