6d3981787b765c58b26038a05206906d53e7de21
karxg <karim44230@gmail.com>
2023-10-19 15:45:32 +0300

    6th

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @s: input string.
+ *
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *str)
+
+{
+	int index = 0;
+
+	while (*(str + index) != '\0')
+	{
+		if ((*(str + index) >= 97) && (*(str + index) <= '122'))
+			*(str + index) = *(str + index) - 32;
+		index++;
+	}
+
+	return (str);
+}
