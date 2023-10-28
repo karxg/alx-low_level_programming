e4dfb1611fcf932149630560810ac1a55a612f9f
karxg <karim44230@gmail.com>
2023-10-28 16:16:09 +0200

    1st

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, followed by a new line.
+ *
+ * @argc: argument count
+ * @argv: array
+ *
+ * Return: 0 Success
+ */
+
+int main(int argc, char const *argv[])
+
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
