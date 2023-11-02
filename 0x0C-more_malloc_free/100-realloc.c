2ecccc6c955a3219c80457284b14ac768914c24c
karxg <karim44230@gmail.com>
2023-11-02 14:28:55 +0200

    Fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -54,11 +54,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		}
 	}
-	/*Copy data from the old block to the new block, up to the minimum size.*/
-	for (i = 0; i < minSize; i++)
-	{
-		*((char *)newBlock + i) = *((char *)ptr + i);
-	}
+
 	free(ptr);
 
 	return (newBlock);
