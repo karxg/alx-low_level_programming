7b89edc5e697349e9e90361b9551860051b65c27
karxg <karim4420@aol.com>
2023-11-16 17:52:24 +0200

    Adv1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* adv - function to execute before main function.
+*/
+
+void adv(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
