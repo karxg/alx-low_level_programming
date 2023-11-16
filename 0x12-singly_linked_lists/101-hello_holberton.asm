0bbd0d2df5976ec656e2b65c545e8a499506966a
karxg <karim4420@aol.com>
2023-11-16 18:12:59 +0200

    Adv2Fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,16 @@
 section .data
-    hello db 'Hello, World', 0xA
+   hello db 'Hello, World', 0xA
 
 section .text
-    global main
-    extern printf
+   global main
+   extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [rel hello]
-    xor rax, rax
-    call printf
-
-    xor eax, eax
-
-    pop rbp
-    ret
+   push rbp
+   mov rbp, rsp
+   lea rdi, [rel hello]
+   xor rax, rax
+   call printf
+   xor eax, eax
+   pop rbp
+   ret
