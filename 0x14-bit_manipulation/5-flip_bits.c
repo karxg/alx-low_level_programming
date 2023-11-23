03683b7e2331feb217220e419570ef01b2ef3f7b
karxg <karim4420@aol.com>
2023-11-23 18:34:42 +0200

    6thFix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int numBits;
 
-	for (numBits = 0; n || m; ((n >>= 1, m >>= 1))
+	for (numBits = 0; n || m; (n >>= 1, m >>= 1))
 	{
 		if ((n & 1) != (m & 1))
 		{
